77929130a8b658110733c1dd78eef0093456e8d3
Steph <stephanwilson10@yahoo.com>
2023-06-27 06:45:46 -0400

    src/app

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import './App.css'
 
  function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 console.log(puppies)
   return (
     <>
      { 
    puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>
+     return <p onClick={} key={puppy.id}>{puppy.name}</p>
    })
    
 }
