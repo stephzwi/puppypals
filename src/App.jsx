dad20f0aadf752b4b7dbb27e3cefc04bdb460970
Steph <stephanwilson10@yahoo.com>
2023-06-29 16:39:13 -0400

    rendered age and email

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import './App.css'
  function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 console.log(puppies)
-
+console.log(featuredPup)
 function handleClick(puppyId) {
   // some logic here
   console.log("puppy id: ", puppyId)
@@ -15,18 +16,34 @@ function handleClick(puppyId) {
 
   console.log("puppyList: ", puppyList);
 
+
+
   return (
     <div className="App">
+    
+     {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       { 
       puppies.map((puppy) => {
-        return <p onClick={()=>handleClick(puppy.id)} key={puppy.id}>
+        return <p onClick={()=>setFeatPupId(puppy.id)} key={puppy.id}>
           {puppy.name}</p>
-          })
         
+          })
+      
         
       }
     </div>
   )
+
+  
 }
+
 
 export default App
